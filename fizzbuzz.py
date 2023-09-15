f07954949ba79ed3c5741a88220084a9ee6c45f0
sooyoung-wind <sooyoung.wind@gmail.com>
2023-09-15 15:13:18 +0900

    feat: print buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,6 +10,8 @@ def do_fizzbuzz():
     for i in range(1,20+1):
         if i%3 == 0:
             print('fizz')
+        elif i%5 ==0:
+            print('buzz')
         else:
             print(i)
 
